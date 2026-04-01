d6ca6642791786f55cf7d4fc021fec18de04ef2c
letydrafts <giovannaleticia02203@gmail.com>
2026-04-01 11:26:29 -0300

    feat: implementando bcrypt

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,11 +1,14 @@
 const { criarUsuario, listarUsuarios } = require('../services/usuarioService');
+const bcrypt = require('bcrypt');
 
 const criar = async (req, res) => {
     const { nome, email, senha, tipo } = req.body;
     if (!nome || !email || !senha || !tipo) return res.status(400)
         .json({ erro: 'nome, email, senha e tipo são obrigatórios'})
 
-    const usuario = await criarUsuario(nome, email, senha, tipo);
+    const senhaHash = await bcrypt.hash(senha, 10);
+
+    const usuario = await criarUsuario(nome, email, senhaHash, tipo);
     res.status(201).json(usuario);
 }
 
