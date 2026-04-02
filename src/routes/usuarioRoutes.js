639bbf0627e594734138a28132d957d3791abd16
letydrafts <giovannaleticia02203@gmail.com>
2026-04-01 21:21:54 -0300

    test: deve retornar um usuário pelo id (GREEN)

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
-const { criar, listar, atualizar } = require('../controllers/usuarioController');
+const { criar, listar, atualizar, buscarPorId } = require('../controllers/usuarioController');
 
 const router = Router();
 
 router.post("/", criar);
 router.get("/", listar);
 router.put("/:id", atualizar);
+router.get("/:id", buscarPorId);
 
 module.exports = router;
