ef7199d211428dc069bcc5dc6881a2255b35b702
letydrafts <giovannaleticia02203@gmail.com>
2026-04-14 18:26:09 -0300

    test: deve registrar uma nova multa (RED)

--- tests/multas.test.js
+++ tests/multas.test.js
@@ -4,5 +4,16 @@ const api = `http://localhost:${process.env.PORT || 3000}`;
 
 describe('Multas', () => {
     
+    test("deve registrar uma nova multa", async () => {
+        const res = await axios.post(`${api}/multas`, {
+            usuario_id: 1,
+            emprestimo_id: 1,
+            dias_atrasados: 5,
+            valor: 10.00,
+            quitado: false,
+        });
+        expect(res.status).toBe(201);
+        expect(res.data).toHaveProperty("id");
+        });
 
 });
