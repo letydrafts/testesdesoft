05a38ec733cd5c938f9f1674d5cd6e2d2e08e645
letydrafts <giovannaleticia02203@gmail.com>
2026-04-15 10:49:11 -0300

    feat: configurando rota de multa

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 const livroRoutes = require("./livroRoutes");
 const usuarioRoutes = require("./usuarioRoutes");
 const emprestimoRoutes = require("./emprestimoRoutes");
+const { Multa } = require("./emprestimoRoutes");
 
 const router = Router();
 router.use("/livros", livroRoutes);
 router.use("/usuarios", usuarioRoutes);
 router.use("/emprestimos", emprestimoRoutes);
+router.use("/multas", require("./multaRoutes"));
 
 module.exports = router;
